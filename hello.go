e844a0fdc0ddd1c229973b845751b981a146a3c0
vikram.choudhary <vikram.choudhary@payu.in>
2018-01-18 12:01:39 +0530

    adding and removing function

---
+++ hello.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func helloHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	//index, err := ioutil.ReadFile("public/index.html")
+	//if err != nil {
+	//	return events.APIGatewayProxyResponse{}, err
+	//}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string("hello world"),
+		Headers: map[string]string{
+			"Content-Type": "text/plain",
+		},
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(helloHandler)
+}
