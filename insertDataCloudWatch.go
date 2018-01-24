944d9c9708d60777a86806df848480965404b444
vikram.choudhary <vikram.choudhary@payu.in>
2018-01-24 15:08:02 +0530

    change function declaration

--- insertDataCloudWatch.go
+++ insertDataCloudWatch.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -10,12 +8,14 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"os"
 	"time"
+	"log"
 )
 
 const TableNameInsertDataCloudWatch = "TABLE_NAME"
 
-func insertEventInDBCloudWatch(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func insertEventInDBCloudWatch(request string)  error{
 
+	log.Println("Running inserEventINDB" + request)
 	region := "us-east-1"
 	tableName := os.Getenv(TableNameInsertDataCloudWatch)
 	awsSession := session.Must(session.NewSession(&aws.Config{
@@ -27,52 +27,20 @@ func insertEventInDBCloudWatch(request events.APIGatewayProxyRequest) (events.AP
 
 	var body map[string]interface{}
 
-	if err := json.Unmarshal([]byte(request.Body), &body); err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       string("Something went wrong"),
-			Headers: map[string]string{
-				"Content-Type": "text/plain",
-			},
-		}, nil
-	}
 	body["eventID"] = time.Now().String()
 
-	dynamoItem, err := dynamodbattribute.MarshalMap(&body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Something went wrong" + err.Error(),
-			Headers: map[string]string{
-				"Content-Type": "text/plain",
-			},
-		}, nil
+	dynamoItem, _ := dynamodbattribute.MarshalMap(&body)
 
-	}
 	putItemInput := &dynamodb.PutItemInput{
 		TableName: aws.String(tableName),
 		Item:      dynamoItem,
 	}
 
-	_, err = dbClient.PutItem(putItemInput)
+	_, err := dbClient.PutItem(putItemInput)
 	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 500,
-			Body:       "Something went wrong" + err.Error(),
-			Headers: map[string]string{
-				"Content-Type": "text/plain",
-			},
-		}, nil
+		log.Println(err.Error())
 	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string("this is main, test 3"),
-		Headers: map[string]string{
-			"Content-Type": "text/plain",
-		},
-	}, nil
-
+	log.Println("Done")
 }
 
 func main() {
